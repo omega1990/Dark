363a7b59673e3b215abedfd57e47191397b74c23
Darko Supe <darko.supe@gmail.com>
2019-05-05 20:35:24 +0200

    Improved AI aiming.

--- Source/Dark/DarkGameMode.cpp
+++ Source/Dark/DarkGameMode.cpp
@@ -9,7 +9,7 @@ ADarkGameMode::ADarkGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
