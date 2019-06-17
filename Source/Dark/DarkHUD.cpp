c9dbf3a6b7e0575a7aec57a094505d78404d2d6a
Darko Supe <darko.supe@gmail.com>
2019-06-17 12:59:07 +0200

    Fixed AI stuck.

--- Source/Dark/DarkHUD.cpp
+++ Source/Dark/DarkHUD.cpp
@@ -10,7 +10,7 @@
 ADarkHUD::ADarkHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Static/Player/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
