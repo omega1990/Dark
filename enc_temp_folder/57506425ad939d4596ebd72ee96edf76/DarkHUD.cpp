d610991420cf986be48c3571ae61295e613ea071
Darko Supe <darko.supe@gmail.com>
2019-06-22 22:41:45 +0200

    Implemented aiming down sight.

--- enc_temp_folder/57506425ad939d4596ebd72ee96edf76/DarkHUD.cpp
+++ enc_temp_folder/57506425ad939d4596ebd72ee96edf76/DarkHUD.cpp
@@ -10,8 +10,8 @@
 ADarkHUD::ADarkHUD()
 {
 	// Set the crosshair texture
-	//static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Static/Player/Textures/FirstPersonCrosshair"));
-	//CrosshairTex = CrosshairTexObj.Object;
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Static/Player/Textures/FirstPersonCrosshair"));
+	CrosshairTex = CrosshairTexObj.Object;
 }
 
 
@@ -25,11 +25,11 @@ void ADarkHUD::DrawHUD()
 	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
 
 	// offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
-	/*const FVector2D CrosshairDrawPosition( (Center.X),
+	const FVector2D CrosshairDrawPosition( (Center.X),
 										   (Center.Y + 20.0f));
-*/
+
 	// draw the crosshair
-	/*FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
+	FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
 	TileItem.BlendMode = SE_BLEND_Translucent;
-	Canvas->DrawItem( TileItem );*/
+	Canvas->DrawItem( TileItem );
 }
